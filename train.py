d9e66669a6a1b3773ad8df16893862e950380f04
yangheng <allchangeworld@gmail.com>
2019-10-26 13:06:16 +0800

    code refactor

--- train.py
+++ train.py
@@ -1,59 +1,51 @@
 # -*- coding: utf-8 -*-
-# file: train.py
-# author: songyouwei <youwei0314@gmail.com>
-# Copyright (C) 2018. All Rights Reserved.
+# file: batch_train.py
+# author: yangheng <yangheng@m.scnu.edu.cn>
+# base code: songyouwei <youwei0314@gmail.com>
+# Copyright (C) 2019. All Rights Reserved.
 
-# modified: yangheng<yangheng@m.scnu.edu.cn>
 
-from pytorch_pretrained_bert import BertModel
+from pytorch_transformers import BertModel
 from sklearn import metrics
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from tensorboardX import SummaryWriter
 import argparse
 import math
 import os
+import numpy, random
 
-from data_utils import build_tokenizer, build_embedding_matrix, Tokenizer4Bert, ABSADataset
+from data_utils import Tokenizer4Bert, ABSADataset, parse_experiments
 
 from models import LCF_BERT
 from models.bert_spc import BERT_SPC
+
+import logging,sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 class Instructor:
     def __init__(self, opt):
         self.opt = opt
+        tokenizer = Tokenizer4Bert(opt.max_seq_len, opt.pretrained_bert_name)
+        bert = BertModel.from_pretrained(opt.pretrained_bert_name)
 
-        if 'bert' in opt.model_name:
-            # opt.learning_rate = 2e-5
-            tokenizer = Tokenizer4Bert(opt.max_seq_len, opt.pretrained_bert_name)
-            bert = BertModel.from_pretrained(opt.pretrained_bert_name)
+        self.model = opt.model_class(bert, opt).to(opt.device)
 
-            self.model = opt.model_class(bert, opt).to(opt.device)
-        else:
-            # opt.learning_rate = 0.001
-            tokenizer = build_tokenizer(
-                fnames=[opt.dataset_file['train'], opt.dataset_file['test']],
-                max_seq_len=opt.max_seq_len,
-                dat_fname='{0}_tokenizer.dat'.format(opt.dataset))
-            embedding_matrix = build_embedding_matrix(
-                word2idx=tokenizer.word2idx,
-                embed_dim=opt.embed_dim,
-                dat_fname='{0}_{1}_embedding_matrix.dat'.format(str(opt.embed_dim), opt.dataset))
-            self.model = opt.model_class(embedding_matrix, opt).to(opt.device)
-        # self.model = nn.DataParallel(self.model)
         trainset = ABSADataset(opt.dataset_file['train'], tokenizer)
         testset = ABSADataset(opt.dataset_file['test'], tokenizer)
         self.train_data_loader = DataLoader(dataset=trainset, batch_size=opt.batch_size, shuffle=True)
         self.test_data_loader = DataLoader(dataset=testset, batch_size=opt.batch_size, shuffle=False)
 
         if opt.device.type == 'cuda':
-            print("cuda memory allocated:", torch.cuda.memory_allocated(device=opt.device.index))
+            logging.info("cuda memory allocated:{}".format(torch.cuda.memory_allocated(device=opt.device.index)))
 
-        self._print_args()
+        self._log_write_args()
 
-    def _print_args(self):
+    def _log_write_args(self):
         n_trainable_params, n_nontrainable_params = 0, 0
         for p in self.model.parameters():
             n_params = torch.prod(torch.tensor(p.shape))
@@ -61,9 +53,9 @@ class Instructor:
                 n_trainable_params += n_params
             else:
                 n_nontrainable_params += n_params
-        print('n_trainable_params: {0}, n_nontrainable_params: {1}'.format(n_trainable_params, n_nontrainable_params))
+        logging.info('n_trainable_params: {0}, n_nontrainable_params: {1}'.format(n_trainable_params, n_nontrainable_params))
         for arg in vars(self.opt):
-            print('>>> {0}: {1}'.format(arg, getattr(self.opt, arg)))
+            logging.info('>>> {0}: {1}'.format(arg, getattr(self.opt, arg)))
 
     def _reset_params(self):
         for child in self.model.children():
@@ -77,13 +69,13 @@ class Instructor:
                             torch.nn.init.uniform_(p, a=-stdv, b=stdv)
 
     def _train(self, criterion, optimizer, max_test_acc_overall=0):
-        writer = SummaryWriter(log_dir=self.opt.logdir)
+
         max_test_acc = 0
         max_f1 = 0
         global_step = 0
         for epoch in range(self.opt.num_epoch):
-            print('>' * 100)
-            print('epoch: ', epoch)
+            logging.info('>' * 100)
+            logging.info('epoch: {}'.format(epoch))
             n_correct, n_total = 0, 0
             for i_batch, sample_batched in enumerate(self.train_data_loader):
                 global_step += 1
@@ -93,9 +85,9 @@ class Instructor:
                 optimizer.zero_grad()
 
                 inputs = [sample_batched[col].to(self.opt.device) for col in self.opt.inputs_cols]
+
                 outputs = self.model(inputs)
                 targets = sample_batched['polarity'].to(self.opt.device)
-
                 loss = criterion(outputs, targets)
                 loss.backward()
                 optimizer.step()
@@ -113,17 +105,12 @@ class Instructor:
                                 os.mkdir('state_dict')
                             path = 'state_dict/{0}_{1}_acc{2}'.format(self.opt.model_name, self.opt.dataset,
                                                                       round(test_acc * 100, 2))
-                            torch.save(self.model.state_dict(), path)
-                            print('>> saved: ' + path)
-                        # print('max_acc:', round(test_acc * 100, 2), 'f1:', round(f1 * 100, 2))
+                            # torch.save(self.model.state_dict(), path)
+                            logging.info('>> saved: ' + path)
+                        logging.info('max_acc:{}  f1:{}'.format(round(test_acc * 100, 2),round(f1 * 100, 2)))
                     if f1 > max_f1:
                         max_f1 = f1
-
-                    writer.add_scalar('loss', loss, global_step)
-                    writer.add_scalar('acc', train_acc, global_step)
-                    writer.add_scalar('test_acc', test_acc, global_step)
-                    print('loss: {:.4f}, acc: {:.2f}, test_acc: {:.2f}, f1: {:.2f}'.format(loss.item(),train_acc*100,test_acc*100, f1*100))
-        writer.close()
+                    logging.info('loss: {:.4f}, acc: {:.2f}, test_acc: {:.2f}, f1: {:.2f}'.format(loss.item(),train_acc*100,test_acc*100, f1*100))
         return max_test_acc, max_f1
 
     def _evaluate_acc_f1(self):
@@ -136,6 +123,7 @@ class Instructor:
                 t_inputs = [t_sample_batched[col].to(self.opt.device) for col in self.opt.inputs_cols]
                 t_targets = t_sample_batched['polarity'].to(self.opt.device)
                 t_outputs = self.model(t_inputs)
+
                 n_test_correct += (torch.argmax(t_outputs, -1) == t_targets).sum().item()
                 n_test_total += len(t_outputs)
 
@@ -151,6 +139,7 @@ class Instructor:
         return test_acc, f1
 
     def run(self, repeats=1):
+
         # Loss and Optimizer
         criterion = nn.CrossEntropyLoss()
         _params = filter(lambda p: p.requires_grad, self.model.parameters())
@@ -159,30 +148,31 @@ class Instructor:
         max_test_acc_overall = 0
         max_f1_overall = 0
         for i in range(repeats):
-            print('repeat: ', i)
+            logging.info('repeat: {}'.format(i))
             self._reset_params()
             max_test_acc, max_f1 = self._train(criterion, optimizer, max_test_acc_overall=max_test_acc_overall)
-            # print('max_test_acc: {0}     max_f1: {1}'.format(max_test_acc, max_f1))
+            # logging.info('max_test_acc: {0}     max_f1: {1}'.format(max_test_acc, max_f1))
 
             max_test_acc_overall = max(max_test_acc, max_test_acc_overall)
             max_f1_overall = max(max_f1, max_f1_overall)
-            print('#' * 100)
+            logging.info('#' * 100)
 
-        print("max_test_acc_overall:", max_test_acc_overall * 100)
-        print("max_f1_overall:", max_f1_overall * 100)
+        logging.info("max_test_acc_overall:{}".format(max_test_acc_overall * 100))
+        logging.info("max_f1_overall:{}".format(max_f1_overall * 100))
         return max_test_acc_overall * 100, max_f1_overall * 100
 
 
-def single_train(model, dataset, local_context_focus, SRD):
+def single_train():
 
-    # Hyper Parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default=model, type=str)
-    parser.add_argument('--dataset', default=dataset, type=str, help='twitter, restaurant, laptop')
+    parser.add_argument('--model_name', default='lcf_bert', type=str)
+    parser.add_argument('--dataset', default='laptop', type=str, help='twitter, restaurant, laptop')
+    parser.add_argument('--use_single_bert', default=None, type=bool,
+                        help='use the same bert modeling for global context and local context to reduce memory requirement')
     parser.add_argument('--optimizer', default='adam', type=str)
     parser.add_argument('--initializer', default='xavier_uniform_', type=str)
-    parser.add_argument('--learning_rate', default=2e-5, type=float)
-    parser.add_argument('--dropout', default=0.1, type=float)
+    parser.add_argument('--learning_rate', default=0.001, type=float)
+    parser.add_argument('--dropout', default=0, type=float)
     parser.add_argument('--l2reg', default=0.00001, type=float)
     parser.add_argument('--num_epoch', default=5, type=int)
     parser.add_argument('--batch_size', default=16, type=int)
@@ -191,19 +181,31 @@ def single_train(model, dataset, local_context_focus, SRD):
     parser.add_argument('--embed_dim', default=300, type=int)
     parser.add_argument('--hidden_dim', default=300, type=int)
     parser.add_argument('--bert_dim', default=768, type=int)
-    parser.add_argument('--pretrained_bert_name', default='bert-base-uncased', type=str)
+    # parser.add_argument('--pretrained_bert_name', default='bert-based-uncased', type=str)
+    # if there are domain adapted pretrained models
+    parser.add_argument('--pretrained_bert_name', default='bert_pretrained_models/laptop', type=str)
+    # parser.add_argument('--pretrained_bert_name', default='bert_pretrained_models/restaurant', type=str)
     parser.add_argument('--max_seq_len', default=80, type=int)
     parser.add_argument('--polarities_dim', default=3, type=int)
     parser.add_argument('--hops', default=3, type=int)
     parser.add_argument('--device', default='cuda', type=str)
-    parser.add_argument('--SRD', default=SRD, type=int)
-    parser.add_argument('--local_context_focus', default=local_context_focus, type=str)
-
+    parser.add_argument('--SRD', default=5, type=int)
+    parser.add_argument('--local_context_focus', default='cdm', type=str)
+    parser.add_argument('--seed', default=2019, type=str)
     opt = parser.parse_args()
+
+    if opt.seed is not None:
+        random.seed(opt.seed)
+        numpy.random.seed(opt.seed)
+        torch.manual_seed(opt.seed)
+        torch.cuda.manual_seed(opt.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     model_classes = {
         'bert_spc': BERT_SPC,
         'lcf_bert': LCF_BERT,
+        'lca_net': LCF_BERT,
     }
 
     dataset_files = {
@@ -219,11 +221,13 @@ def single_train(model, dataset, local_context_focus, SRD):
             'train': './datasets/semeval14/Laptops_Train.xml.seg',
             'test': './datasets/semeval14/Laptops_Test_Gold.xml.seg'
         }
+
     }
 
     input_colses = {
-        'bert_spc': ['text_bert_indices', 'bert_segments_ids', 'aspect_indices'],
+        'bert_spc': ['text_bert_indices', 'bert_segments_ids'],
         'lcf_bert': ['text_bert_indices', 'bert_segments_ids', 'text_raw_bert_indices', 'aspect_bert_indices'],
+        'lca_net': ['text_bert_indices', 'bert_segments_ids', 'text_raw_bert_indices', 'aspect_bert_indices'],
     }
     initializers = {
         'xavier_uniform_': torch.nn.init.xavier_uniform_,
@@ -244,6 +248,7 @@ def single_train(model, dataset, local_context_focus, SRD):
     opt.inputs_cols = input_colses[opt.model_name]
     opt.initializer = initializers[opt.initializer]
     opt.optimizer = optimizers[opt.optimizer]
+
     opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') \
         if opt.device is None else torch.device(opt.device)
 
@@ -251,73 +256,6 @@ def single_train(model, dataset, local_context_focus, SRD):
     return ins.run()  # _reset_params in every repeat
 
 
-def multi_train(model, dataset, SRD, local_context_focus, n):
-    mean_test_acc_overall = 0
-    mean_f1_overall = 0
-    temp_test_acc_overall = 0
-    temp_f1_overall = 0
-    max_acc_overall = 0
-    max_f1_overall = 0
-    scores = []
-    for t in range(n):
-        print(model, '-', dataset, '-', local_context_focus, ' No.', t + 1, ' in ', n)
-        test_acc_overall, f1_overall = single_train(
-                model=model, dataset=dataset,
-                local_context_focus=local_context_focus,
-                SRD=SRD
-        )
-        scores.append([test_acc_overall, f1_overall])
-        temp_test_acc_overall += test_acc_overall
-        temp_f1_overall += f1_overall
-        print("\n\nResults:")
-        for i in range(len(scores)):
-            if scores[i][0] > max_acc_overall:
-                max_acc_overall = scores[i][0]
-                max_f1_overall = scores[i][1]
-            print(i + 1, " test_acc_overall: ", round(scores[i][0], 2), "    f1_overall: ", round(scores[i][1], 2))
-        mean_test_acc_overall = temp_test_acc_overall / (t + 1)
-        mean_f1_overall = temp_f1_overall / (t + 1)
-        print('max_acc_overall:', round(max_acc_overall, 2), 'f1_overall:', round(max_f1_overall, 2))
-        print("mean_acc_overall:", round(mean_test_acc_overall, 2), "mean_f1_overall:", round(mean_f1_overall, 2))
-
-    return mean_test_acc_overall, mean_f1_overall
-
-
 if __name__ == '__main__':
 
     single_train()
-
-    # model = 'lcf_bert'
-    # # model = 'bert_spc'
-    #
-    #
-    # local_context_focus = 'cdm'
-    # local_context_focus='cdw'
-    #
-    # # train times
-    # n = 5
-    #
-    # SRD = 3
-    # mean_test_acc_overall, mean_f1_overall = multi_train(
-    #     model=model, dataset='laptop',
-    #     local_context_focus=local_context_focus,
-    #     SRD=SRD,
-    #     n=n
-    # )
-    #
-    # SRD = 3
-    # mean_test_acc_overall, mean_f1_overall = multi_train(
-    #     model=model, dataset='restaurant',
-    #     local_context_focus=local_context_focus,
-    #     SRD=SRD,
-    #     n=n
-    # )
-    #
-    # SRD = 3
-    # mean_test_acc_overall, mean_f1_overall = multi_train(
-    #     model=model, dataset='twitter',
-    #     local_context_focus=local_context_focus,
-    #     SRD=SRD,
-    #     n=n
-    # )
-    #
